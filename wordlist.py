bb3b85a8867e12e2a402bbe8887650bdb3acd06c
ALP KIRTILLI <76229088+alp1903@users.noreply.github.com>
2021-09-26 22:56:12 +0300

    Add files via upload

---
+++ wordlist.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+import random
+import time
+import os
+
+print(" ____      _    ____  ____ ___ _____")
+print("|  _ \    / \  | __ )| __ )_ _|_   _|")
+print("| |_) |  / _ \ |  _ \|  _ \| |  | |")
+print("|  _ <  / ___ \| |_) | |_) | |  | |")
+print("|_| \_\/_/   \_\____/|____/___| |_|")
+print("        Make by RABBIT")
+print("      --(:HELLO WORLD:)--")
+
+ad = input("[+] Kişinin Adı >>")
+file = open(ad+".txt", "a")
+soyad = input("[+] Kişinin Soyadı >>")
+dtarih = input("[+] Kişinin Doğum Tarihi >>")
+adk = input("[+] Kişinin Ad Kısaltması >>")
+soyadk = input("[+] Kişinin Soyad Kısaltması >>")
+tkm = input("[+] Kişinin Tuttuğu Takım >>")
+th = input("[+] Kişinin Tuttuğu Takım`ın tarihi >>")
+pkod = input("[+] Kişinin Şehrin Plaka Kodu >>")
+hyvn = input("[+] Kişinin Hayvanının Adı >>")
+teln = input("[+] Kişinin telefon nosu >>")
+anahtar1 = input("[+] Kişi Hakkında Anahtar Kelime >>")
+anahtar2 = input("[+] Kişi Hakkında Anahtar Kelime >>")
+anahtar3 = input("[+] Kişi Hakkında Anahtar Kelime >>")
+anahtarr1 = input("[+] Kişi Hakkında Anahtar Rakam >>")
+anahtarr2 = input("[+] Kişi Hakkında Anahtar Rakam >>")
+anahtarr3 = input("[+] Kişi Hakkında Anahtar Rakam >>")
+print("")
+print("[!] İşlem Tamamlanıyor Lütfen Bekleyiniz")
+time.sleep(0.7)
+print("[!] Bilgiler Sisteme Kaydediliyor")
+time.sleep(0.7)
+print("[!] İşlem Tamamlandı")
+time.sleep(0,3)
+#Alp
+liste = [ad, soyad, dtarih, adk, soyadk, tkm, pkod, hyvn, anahtar1, anahtar2, anahtar3, anahtarr1, anahtarr2, anahtarr3, th, teln]
+print("[!] Lütfen 3 Saniye Bekleyiniz")
+time.sleep(0,3)
+op = 1
+while op <= 1200:
+	print(random.choice(liste) + random.choice(liste))
+	file.writelines(random.choice(liste) + random.choice(liste) + "\n")
+	print(random.choice(liste) + random.choice(liste))
+	file.writelines(random.choice(liste) + random.choice(liste) + "\n")
+	print(random.choice(liste) + random.choice(liste))
+	file.writelines(random.choice(liste) + random.choice(liste) + "\n")
+	print(random.choice(liste) + random.choice(liste) + random.choice(liste))
+	file.writelines(random.choice(liste) + random.choice(liste) + random.choice(liste) + "\n")
+	print(random.choice(liste) + random.choice(liste) + random.choice(liste))
+	file.writelines(random.choice(liste) + random.choice(liste) + random.choice(liste) + "\n")
+	print(random.choice(liste) + random.choice(liste) + random.choice(liste))
+	file.writelines(random.choice(liste) + random.choice(liste) + random.choice(liste) + "\n")
+	print(random.choice(liste) + random.choice(liste) + random.choice(liste) + random.choice(liste))
+	file.writelines(random.choice(liste) + random.choice(liste) + random.choice(liste) + random.choice(liste) + "\n")
+	print(random.choice(liste) + random.choice(liste) + random.choice(liste) + random.choice(liste))
+	file.writelines(random.choice(liste) + random.choice(liste) + random.choice(liste) + random.choice(liste) + "\n")
+	print(random.choice(liste) + random.choice(liste) + random.choice(liste) + random.choice(liste))
+	file.writelines(random.choice(liste) + random.choice(liste) + random.choice(liste) + random.choice(liste) + "\n")
+	op = op + 1
+
+file.close()
+os.system("clear")
+os.system("sort -u {}.txt | uniq >> {}1.txt".format(ad, ad))
+os.system("rm {}.txt".format(ad))
